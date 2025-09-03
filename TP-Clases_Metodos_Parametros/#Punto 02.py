3269e8bba9a61244ca0bb8291fe4bd5e733d4361
Ramiro Quiroga <quirogarami11@gmail.com>
2025-09-03 20:33:17 -0300

    Subo punto 2

---
+++ TP-Clases_Metodos_Parametros/#Punto 02.py
@@ -0,0 +1,4 @@
+#2
+#Si se asigna un valor a una variable fuera de rango, normalmente no ocurre un error automáticamente, ya que Python no limita el rango de los valores de las variables.
+#Sin embargo, si el rango es definido explícitamente se puede producir un error como IndexError (si se accede a una posición fuera de una lista) o lógica incorrecta.
+#Para evitarlo, se puede validar el valor antes de usarlo.
