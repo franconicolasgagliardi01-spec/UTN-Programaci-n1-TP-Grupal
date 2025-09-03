ebc2999ecd409e4662bc5acd3e0ff2812258b2bd
Ramiro Quiroga <quirogarami11@gmail.com>
2025-09-03 20:35:16 -0300

    Subo punto 15

---
+++ TP-Clases_Metodos_Parametros/#Punto 15.py
@@ -0,0 +1,3 @@
+#15
+# En el caso de realizar la siguiente declaración de variable: x = None print(x) Python lanzará error, ya que espera solo una instrucción por línea de código.
+# En python none es utilizado para representar la ausencia de valor (es decir, un valor nulo). Es usado cuando una variable no tiene un valor asignado o una función no retorna nada explícitamente.
