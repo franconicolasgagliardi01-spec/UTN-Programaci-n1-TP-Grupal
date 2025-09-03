8b56cd9874a2e52c2c7d56a4d59daf21b2e73fa6
Ramiro Quiroga <quirogarami11@gmail.com>
2025-09-03 20:04:20 -0300

    Subo punto 1

---
+++ TP-Clases_Metodos_Parametros/Punto 1.py
@@ -0,0 +1,8 @@
+#1
+valorDecimal = float(input("Ingrese un número decimal: "))
+valorEntero = int(valorDecimal)
+print(f"El valor entero es: {valorEntero}")
+valorString = str(valorDecimal)
+print(f"El valor string es: {valorString}")
+valorBooleano = bool(valorDecimal)
+print(f"El valor booleano es: {valorBooleano}")
