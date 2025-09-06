0339b97ccfc3de1c8feb97a80c0f00f6aa16af2f
Franco <franconicolasgagliardi01@gmail.com>
2025-09-06 15:49:38 -0300

    Subo Parte1-Punto3 TP Listas

---
+++ TP-Listas/#Parte 1 - Punto 3.py
@@ -0,0 +1,19 @@
+#Parte 1 - Punto 1
+lista = []
+salida = False
+
+while salida == False:
+    elemento = input("Ingrese un elemento a la lista: ")
+    lista.append(elemento)
+    salir = input("Desea seguir agregando elementos? s/n: ")
+    while salir != "s" and salir != "n":
+        print("INGRESE UN VALOR VALIDO")
+        salir = input("Desea seguir agregando elementos? s/n: ")
+    if salir == "n":
+        salida = True
+    else:
+        pass
+
+print(f"Lista normal: {lista}")
+lista.reverse()
+print(f"Lista al reves: {lista}")
