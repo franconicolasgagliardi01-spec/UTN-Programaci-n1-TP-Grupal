63d56ebfb74c137c8ab9c2a52b42c5f3d6b840e1
Franco <franconicolasgagliardi01@gmail.com>
2025-09-06 16:55:26 -0300

    Subo Parte2-Punto2y3 TP Listas

---
+++ TP-Listas/#Parte 2 - Punto 2y3.py
@@ -0,0 +1,12 @@
+#Parte 2 - Punto 2y3
+lista_bidimencional = [[2,4,1],[6,74,22],[31,87,3]]
+
+fila1 = sum(lista_bidimencional[0])
+fila2 = sum(lista_bidimencional[1])
+fila3 = sum(lista_bidimencional[2])
+
+print(f"la suma de los elementos de la primer fila es: {fila1}")
+print(f"la suma de los elementos de la segunda fila es: {fila2}")
+print(f"la suma de los elementos de la tercer fila es: {fila3}")
+
+print(f"la suma de los elementos de la matriz bidimencional es: {fila1+fila2+fila3}")
